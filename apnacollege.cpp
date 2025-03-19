ead17c4de19fe827f9f84c75cf01421e25ed2f7c
mantashasheikh <mantashasheikh95844@gmail.com>
2025-03-19 17:23:47 +0530

    write a program to print reverse of a  number

--- apnacollege.cpp
+++ apnacollege.cpp
@@ -9,21 +9,32 @@ int main(){
     // SI=P*R*T;
     // cout<<SI;
     // check the number is prime or not
-    int n ;
-    cout<<"enter a number:";
-    cin>>n;
-    int count=0;
-    for(int i=1;i<=n;i++){
-       if(n%i==0){
-        count++;
-       }
+    // int n ;
+    // cout<<"enter a number:";
+    // cin>>n;
+    // int count=0;
+    // for(int i=1;i<=n;i++){
+    //    if(n%i==0){
+    //     count++;
+    //    }
       
-    }
-    if(count==2){
-        cout<<n<<":is prime";
-    }else{
-        cout<<n<<":is not prime";
-    }
+    // }
+    // if(count==2){
+    //     cout<<n<<":is prime";
+    // }else{
+    //     cout<<n<<":is not prime";
+    // }
+
+
+    // write a program to print reverse of a number
+    int n,rev=0, digit;
+    cout<<"enter a number: ";
+    cin>>n;
+    while(n>0){
+        digit=n%10;
+        rev=rev*10+digit;
+        n=n/10;
+    }cout<<rev;
 
     }
 
